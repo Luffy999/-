aa79df07e94d5c68465adcda7d838cfbc1573487
Luffy999 <578369270@qq.com>
2017-06-26 15:14:50 +0800

    Update ikea_img_get.py

--- ikea_img_get.py
+++ ikea_img_get.py
@@ -8,16 +8,20 @@
 from bs4 import BeautifulSoup
 import requests
 
+# 获取图片链接
 def get_urls(url):
     html_doc=requests.get(url)
     html_text=BeautifulSoup(html_doc.text,'lxml')
     img_urls=html_text.find_all('img',class_="imageWrapper")
     return img_urls
 
+# 下载图片
 def get_img(url):
     img_urls = get_urls(url)
     for img_url in img_urls:
+        # 拼接成高清图片的链接
         img_src='http://www.ikea.com'+img_url['src'][:-19]+img_url['src'][-13:]
+        # 保存图片
         with open('picture_1/'+img_url['title']+'.jpg','wb') as f:
             f.write(requests.get(img_src).content)
 

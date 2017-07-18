da7bfe4edb64e68c3529c76e56422b3553359598
Luffy999 <578369270@qq.com>
2017-07-18 13:52:58 +0800

    Update SQL_table_create.py

--- SQL_table_create.py
+++ SQL_table_create.py
@@ -1,14 +1,22 @@
 # -*- coding:utf-8 -*-
+# ------------------------------------------------------
+#   版本：py2.7
+#   日期：2017-06-23
+#   作者：Luffy
+# ------------------------------------------------------
+
 import mysql.connector
 
+# 连接数据库
 conn = mysql.connector.connect(
-    host='localhost',
-    port=3306,
-    user='******',
-    password='******',
-    database='******',
+    host='localhost',# 服务器名
+    port=3306,# 端口
+    user='******',# 用户名
+    password='******',# 密码
+    database='******',# 数据库名
 )
 cursor = conn.cursor()
+# 设置字段属性
 cursor.execute('create table doubanmovie_TOP250 '
                '(id int NOT NULL AUTO_INCREMENT,'
                '电影名称 varchar(255),'
@@ -28,6 +36,7 @@ cursor.execute('create table doubanmovie_TOP250 '
                '1星 varchar(24),'
                '好于 varchar(255),'
                'primary key (id,电影名称))')
+# 断开连接，并关闭数据库
 cursor.close()
 conn.commit()
 conn.close()

e6bf75093a9b5d69e08aa35f10d72b912d135655
Luffy999 <578369270@qq.com>
2017-07-18 13:43:09 +0800

    Update SQL_table_insert.py

--- SQL_table_insert.py
+++ SQL_table_insert.py
@@ -4,9 +4,9 @@ import mysql.connector
 conn = mysql.connector.connect(
     host='localhost',
     port=3306,
-    user='root',
-    password='root',
-    database='article_url',
+    user='******',
+    password='******',
+    database='******',
 )
 cursor = conn.cursor()
 cursor.execute('insert into 邕宁食药局 (id,标题,时间,内容) values (%s,%s,%s,%s)',[2,'title','time','content'])

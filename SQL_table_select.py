c01ce87c895343e70ca30307fb71e0ca4e428b84
Luffy999 <578369270@qq.com>
2017-07-18 13:43:37 +0800

    Update SQL_table_select.py

--- SQL_table_select.py
+++ SQL_table_select.py
@@ -4,9 +4,9 @@ import mysql.connector
 conn = mysql.connector.connect(
     host='localhost',
     port=3306,
-    user='root',
-    password='root',
-    database='article_url',
+    user='******',
+    password='******',
+    database='******',
 )
 cursor = conn.cursor()
 cursor.execute('select title from 广西_南宁_青秀_政府办_政务动态 where title like "%美%京%"')

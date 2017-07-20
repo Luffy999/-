d028ff3ab7dad7142a67a82e6a4608ae3a2d3669
Luffy999 <578369270@qq.com>
2017-07-20 09:40:43 +0800

    Update ajax_geturl_and_import_database.py

--- ajax_geturl_and_import_database.py
+++ ajax_geturl_and_import_database.py
@@ -15,7 +15,7 @@ import re
 # 使用无头浏览器获取URL
 def url_get(page_down,page,URLnumber,url):
     # 启动无头浏览器
-    driver = webdriver.Chrome(executable_path='******') # 浏览器名称和路径
+    driver = webdriver.Chrome(executable_path='E:/chromedriver.exe') # 浏览器名称和路径
     driver.get(url)
     #driver.find_element_by_css_selector('#pageNum > span:nth-child(2)').click()
     driver.implicitly_wait(5)
@@ -24,12 +24,13 @@ def url_get(page_down,page,URLnumber,url):
     while page_down <= page:
         print u'正在爬取第 -' + str(page_down) + u'- 页'
         driver.implicitly_wait(5)
+        time.sleep(3)
         for i in range(1, URLnumber + 1):
            # 逐条获取页面URL
             try:
                 url_gets = driver.find_element_by_css_selector(
-                    '#middle02_r > div.list02 > ul > li:nth-child(' + str(i * 2) + ') > a').get_attribute('href')
-                time.sleep(0.5)
+                    '#c_20118567463596 > table > tbody > tr:nth-child('+str(i*2-1)+') > td:nth-child(1) > a').get_attribute('href')
+                #time.sleep(0.5)
                 print url_gets
                 # 获取信息并导入数据库
                 title,ctime,content = information_get(url_gets)
@@ -40,11 +41,11 @@ def url_get(page_down,page,URLnumber,url):
         #driver.implicitly_wait(5)
         try:
             page_down = page_down + 1  # 页数加1
-            driver.find_element_by_css_selector('#ctl00\\24 ContentPlaceHolder_middle\\24 AspNetPager1_input > option:nth-child(' + str(page_down) + ')').click()
+            driver.find_element_by_css_selector('#p_20118567463596 > table > tbody > tr > td > a:nth-child(2)').click()
             #if page_down == 2:
-                #driver.find_element_by_css_selector('#fanye > table > tbody > tr > td:nth-child(1) > a:nth-child(1)').click()
+            #    driver.find_element_by_css_selector('#list_wrap_tpl > div.pg-3 > a:nth-child(6)').click()
             #else:
-                #driver.find_element_by_css_selector('#fanye > table > tbody > tr > td:nth-child(1) > a:nth-child(3)').click()
+            #    driver.find_element_by_css_selector('#list_wrap_tpl > div.pg-3 > a:nth-child(38)').click()
         except Exception,e:
             print e
             if 'no such element' in str(e):
@@ -60,27 +61,27 @@ def information_get(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3080.5 Safari/537.36'}
     html_content = BeautifulSoup(requests.get(url, headers=headers).text.encode('latin1'), 'lxml')
-    title = re.sub(r'\s','',html_content.find('h3',{'align':'center'}).text) # 标题
-    time = re.compile(r'(\d{4}-\d{1,2}-\d{1,2})').search(html_content.find_all('td',{'align':'center'})[2].text).group()  # 时间
-    content = re.sub(r'\s','',html_content.find_all('td',{'class':'font'})[1].text) # 内容
+    title = re.sub(r'\s','',html_content.find('div',{'class':'article_title'}).text) # 标题
+    time = re.compile(ur'(\d{4}年\d{1,2}月\d{1,2}日)').search(html_content.find_all('div',{'class':'author'})[0].text).group()  # 时间
+    content = re.sub(r'\s','',html_content.find('div',{'class':'text_content'}).text) # 内容
     return title,time,content
 
 # 连接数据库并导入数据
 def database_connect(title,ctime,content):
     conn = mysql.connector.connect(
-        host = 'localhost',# 服务器名称
+        host = '******',# 服务器名称
         user = '******',# 用户名
         password = '******',# 密码
         database = '******'# 数据库名称
     )
     cursor = conn.cursor()
     # 尝试导入数据，如数据已经存在则跳过
-    cursor.execute('insert into 广西_防城港_市级_环保局_县区动态'
+    cursor.execute('insert into 吉林_辽源_东辽_政府办_东辽新闻'
                    '(title,time,content) values (%s,%s,%s)',[title,ctime,content])
     cursor.close()
     conn.commit()
     conn.close()
 
 if __name__ == '__main__':
-    url = 'http://fcghbj.gov.cn/ChannelMess/List.aspx?Channel_ID=7'
-    url_get(1,3,20,url) # 参数以此为起始页数、总页数、每个页面内URL数量、起始页的URL
+    url = 'http://www.dongliao.gov.cn/Item/list.asp?id=1443'
+    url_get(1,181,20,url) # 参数以此为起始页数、总页数、每个页面内URL数量、起始页的URL
